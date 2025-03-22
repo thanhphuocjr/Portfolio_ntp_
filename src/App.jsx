657676c2ca2c62d7b6b9266f31e04e5863b3848a
thanhphuocjr <thanhphuocjr03012005@gmail.com>
2025-03-22 22:47:06 +0700

    Update Cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./app.scss";
 import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/hero";
 import { Navbar } from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
@@ -8,6 +9,7 @@ import Services from "./components/services/Services";
 const App = () => {
   return (
     <div>
+      <Cursor />
       <section id="HomePage">
         <Navbar />
         <Hero />
