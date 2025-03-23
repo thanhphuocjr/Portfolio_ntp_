6be5e1205709abe2f8f567795349a3430432698c
thanhphuocjr <thanhphuocjr03012005@gmail.com>
2025-03-23 14:05:15 +0700

    fix hero to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./app.scss";
 import Contact from "./components/contact/Contact";
 import Cursor from "./components/cursor/Cursor";
-import Hero from "./components/hero/hero";
+import Hero from "./components/hero/Hero";
 import { Navbar } from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
